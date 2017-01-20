7e3e699c53769842451b98576cebe009199f391f
zhangzh <zhangzh@5607bd42-195c-0410-82fb-b7440d194b30>
2017-01-20 09:18:07 +0000

    修改了敏感词算法，富文本不检测标签，只检测文本
    git-svn-id: https://svn.chsi.com.cn/svn/repos/chsicommunity/knowledge@25271 5607bd42-195c-0410-82fb-b7440d194b30

--- trunk/knowledge/src/main/java/com/chsi/knowledge/service/impl/FilterWordServiceImpl.java
+++ trunk/knowledge/src/main/java/com/chsi/knowledge/service/impl/FilterWordServiceImpl.java
@@ -7,13 +7,12 @@ import java.util.List;
 import java.util.Set;
 
 import com.chsi.framework.service.BaseDbService;
-import com.chsi.framework.util.StringUtil;
 import com.chsi.gxjx.tool.WordsFilter;
 import com.chsi.knowledge.service.FilterWordService;
 import com.chsi.knowledge.util.SearchUtil;
-import com.sun.jndi.toolkit.dir.SearchFilter;
 
 public class FilterWordServiceImpl extends BaseDbService implements FilterWordService {
+    private static final String HIGHLIGHT_FORMAT = "<span style=\"color:#F00;font-style:italic;\">%s</span>";
 
     @Override
     protected void doCreate() {
@@ -28,26 +27,23 @@ public class FilterWordServiceImpl extends BaseDbService implements FilterWordSe
     }
 
     @Override
-    public Set<String> getBadWords(String text) throws UnsupportedEncodingException {
-        // TODO Auto-generated method stub
-        return WordsFilter.getInstance().getBadWords(text);
+    public Set<String> getBadWords(String html) throws UnsupportedEncodingException {
+        return WordsFilter.getInstance().getHtmlBadWords(html);
     }
 
     @Override
     public Object[] highlightBadWords(String text) throws UnsupportedEncodingException {
-        // TODO Auto-generated method stub
-        return WordsFilter.getInstance().highlightBadWords(text);
+        return WordsFilter.getInstance().highlightBadWords(text, HIGHLIGHT_FORMAT);
     }
 
     @Override
     public List<String> getBadSentences(String keywords) throws UnsupportedEncodingException {
-        // TODO Auto-generated method stub
         keywords = SearchUtil.resultFilter(keywords);
         List<String> sentences = splitContent(keywords);
         List<String> result = new ArrayList<String>();
         WordsFilter wordsFilter = WordsFilter.getInstance();
         for (String sentence : sentences) {
-            Object[] obj = wordsFilter.highlightBadWords(sentence);
+            Object[] obj = wordsFilter.highlightBadWords(sentence, HIGHLIGHT_FORMAT);
             if ((Boolean) obj[0]) {
                 result.add((String) obj[1]);
             }
