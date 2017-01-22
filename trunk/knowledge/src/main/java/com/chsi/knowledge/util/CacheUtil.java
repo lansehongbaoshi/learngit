a4f94cfe4a65041d411083daf89cd28a3d4254af
shikf <shikf@5607bd42-195c-0410-82fb-b7440d194b30>
2017-01-22 02:54:40 +0000

    查找正式环境读取缓存为空问题
    git-svn-id: https://svn.chsi.com.cn/svn/repos/chsicommunity/knowledge@25292 5607bd42-195c-0410-82fb-b7440d194b30

--- trunk/knowledge/src/main/java/com/chsi/knowledge/util/CacheUtil.java
+++ trunk/knowledge/src/main/java/com/chsi/knowledge/util/CacheUtil.java
@@ -8,7 +8,9 @@ public class CacheUtil {
     
     public static Map<String, Integer> addVisitKnowledgeCache(String knowledgeId){
         Cache cache = Cache.getCache();
+        System.out.println("获取自定义缓存："+cache);
         Map<String, Integer> visitKnowledgeCache = cache.getVisitKnowledgeCache();
+        System.out.println("放置访问数量的map缓存："+visitKnowledgeCache+";数量："+visitKnowledgeCache.size());
         if(visitKnowledgeCache.containsKey(knowledgeId)){
             Integer count = visitKnowledgeCache.get(visitKnowledgeCache);
             visitKnowledgeCache.put(knowledgeId, count+1);
